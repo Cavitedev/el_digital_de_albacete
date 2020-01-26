8f3c485b7f18cac4a1f83ef0109705aac5494e56
Carlos Víllora Tercero <carlosvt@unicarlos.com>
2020-01-26 13:29:57 +0100

    Refactoring Spider Page

--- lib/Spider/SpiderPage.dart
+++ lib/Spider/SpiderPage.dart
@@ -1,8 +1,6 @@
 import 'dart:convert';
 
 import 'package:el_digital_de_albacete/Models/NewsData.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:html/dom.dart' as dom;
 import 'package:html/parser.dart' as parser;
 import 'package:http/http.dart' as http;
@@ -13,14 +11,21 @@ class SpiderPage {
   SpiderPage({this.url});
 
 
-  
   Future<List<NewsData>> scrapPage() async {
+    dom.Document _document = await _accessURL(url);
+    return _getNews(_document);
+  }
+  
+  Future<dom.Document> _accessURL(String _url) async {
     http.Response response =
-        await http.get("https://www.eldigitaldealbacete.com/economia-2/");
+    await http.get(url);
     String body = utf8.decode(response.bodyBytes);
     dom.Document document = parser.parse(body);
+    return document;
+  }
+  List<NewsData> _getNews(dom.Document _document){
     List<dom.Element> articles =
-        document.body.getElementsByClassName("post-listing")[0].children;
+        _document.body.getElementsByClassName("post-listing")[0].children;
     List<NewsData> news = List<NewsData>();
     for (dom.Element article in articles) {
       dom.Element anchor = article.children[0].children[0];
@@ -30,8 +35,11 @@ class SpiderPage {
         imageSrc: anchor.children[0].attributes['src'],
         publishDate: article.children[2].children[0].text,
       ));
-    //  debugPrint(news.last.title);
+      //  debugPrint(news.last.title);
     }
     return news;
   }
+
+  
+
 }
