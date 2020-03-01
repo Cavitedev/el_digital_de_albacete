b5127a18114ac66ba6aaed88be7139eda92d922d
Carlos Víllora Tercero <carlosvt@unicarlos.com>
2020-03-01 15:37:22 +0100

    improve singleSpider performance

--- lib/Spider/SpiderSingleNews.dart
+++ lib/Spider/SpiderSingleNews.dart
@@ -10,16 +10,18 @@ class SpiderSingleNews extends Spider {
 
   SpiderSingleNews({this.url});
 
-  static const List<String> _contentClasses = <String>["NormalTextoNoticia","entry"];
+  static const List<String> _contentClasses = <String>[
+    "NormalTextoNoticia",
+    "entry"
+  ];
   static const String _unworthText = "/Redacción/";
 
   Future<ExtraNewsData> scrapSingleNewsPage() async {
     dom.Document _document = await accessURL(url);
     List<dom.Element> _entryDatas;
-    for(String _contentClass in _contentClasses){
-      _entryDatas =
-          _document.getElementsByClassName(_contentClass);
-      if(_entryDatas.isNotEmpty) break;
+    for (String _contentClass in _contentClasses) {
+      _entryDatas = _document.getElementsByClassName(_contentClass);
+      if (_entryDatas.isNotEmpty) break;
     }
     _entryDatas = _entryDatas[0].children;
     List<NewsData> newsInformation = List<NewsData>();
@@ -30,38 +32,42 @@ class SpiderSingleNews extends Spider {
             _data.children[0].attributes.isNotEmpty) {
           String _imageUrl = _data.children[0].attributes['src'];
           if (_imageUrl != null) {
-            newsInformation.add(
-                MeaningfulString(string:_imageUrl, textTag: TextTag.img));
+            newsInformation
+                .add(MeaningfulString(string: _imageUrl, textTag: TextTag.img));
           }
-        }
-        String _text = _data.text;
+        } else {
+          String _text = _data.text;
 
-        if (_text.trim().isNotEmpty && _text!=_unworthText) {
-          newsInformation.add(
-              MeaningfulString(string: _text, textTag: TextTag.p));
-        }
-      } else if (_data.localName == "h2") {
-        newsInformation.add(
-            MeaningfulString(string: _data.text, textTag: TextTag.h2));
-      }else if(_data.localName == "table"){
-        List<String> headers = List<String>();
-        for(dom.Element td in _data.children[0].children[0].children){
-          headers.add(td.text);
-        }
-      List<List<dynamic>> table = List<List<dynamic>>();
-        for(int i = 1; i<_data.children[0].children.length; i++){
-          dom.Element tr = _data.children[0].children[i];
-          List<dynamic> row = List<dynamic>();
-          for(dom.Element td in tr.children){
-            row.add(td.text);
+          //print(_data.children.length>0?_data.children[0]?.text:"");
+          if (_text.trim().isNotEmpty && _text != _unworthText) {
+
+            newsInformation
+                .add(MeaningfulString(string: _text, textTag: TextTag.p));
+
+          } else if (_data.localName == "h2") {
+            newsInformation
+                .add(MeaningfulString(string: _data.text, textTag: TextTag.h2));
           }
-          table.add(row);
+          else if (_data.localName == "table") {
+            List<String> headers = List<String>();
+            for (dom.Element td in _data.children[0].children[0].children) {
+              headers.add(td.text);
+            }
+            List<List<dynamic>> table = List<List<dynamic>>();
+            for (int i = 1; i < _data.children[0].children.length; i++) {
+              dom.Element tr = _data.children[0].children[i];
+              List<dynamic> row = List<dynamic>();
+              for (dom.Element td in tr.children) {
+                row.add(td.text);
+              }
+              table.add(row);
+            }
+            newsInformation.add(DataOfTable(table, headers));
+          }
         }
-        newsInformation.add(DataOfTable(table, headers));
+        //
       }
-      //
     }
-
     return ExtraNewsData(newsContent: newsInformation);
   }
 }
