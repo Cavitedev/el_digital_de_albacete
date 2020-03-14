a44677de411bd8fe8c8ba13ab9d0b0d25ca69ba7
Carlos Víllora Tercero <carlosvt@unicarlos.com>
2020-03-14 18:15:24 +0100

    changed some font sizes

--- lib/main.dart
+++ lib/main.dart
@@ -21,11 +21,11 @@ class MyApp extends StatelessWidget {
           headline: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, ),
           body1: TextStyle(fontSize: 20,),
           caption: TextStyle(fontSize: 10, color: Colors.black54, ),
-          title: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, ),
-          display1: TextStyle(fontSize: 30, color: Colors.white, ),
+          title: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, ),
+          display1: TextStyle(fontSize: 30, color: Colors.white, fontFamily: "NotoSans"),
           subhead: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, ),
           subtitle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, ),
-          button: TextStyle(fontSize: 20, color: Colors.white, )
+          button: TextStyle(fontSize: 20, color: Colors.white, fontFamily: "NotoSans")
         ),
       ),
       home: NewsList(),
