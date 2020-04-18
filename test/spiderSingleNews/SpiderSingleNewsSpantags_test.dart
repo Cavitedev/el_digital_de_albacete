a0afc168ed563ff0741885fc7d25770c74daff65
Carlos Víllora Tercero <carlosvt@unicarlos.com>
2020-04-18 17:52:24 +0200

    Fix spans delete

--- test/spiderSingleNews/SpiderSingleNewsSpantags_test.dart
+++ test/spiderSingleNews/SpiderSingleNewsSpantags_test.dart
@@ -17,9 +17,9 @@ void main(){
       outputData = await SpiderSingleMethods.setUpScrapping(spiderSingleNews);
 
       expect((outputData.newsContent[0] as ParagraphStyledData).styledData[0],StyledString(extraStyle:ParagraphStyledData.attributesStyles['strong'] ,text: "La Comisión de Evaluación del Programa de Apoyo a la Inversión Industrial (Reindus), dependiente del Ministerio de Industria, Comercio y Turismo, ha adjudicado de manera provisional 15.830.569 euros a 12 proyectos industriales de Castilla-La Mancha correspondientes a la convocatoria de 2019."));
-      expect((outputData.newsContent[11] as ParagraphStyledData).styledData[0],StyledString(text: "–GRAFICAS IMPAR S.L. Inversiones en bienes de equipo para la mejora y ampliación de líneas de producción. Toledo. 154.400 €"));
-      expect((outputData.newsContent[14] as ParagraphStyledData).styledData[0],StyledString(text: "–LCN AUTOMOTIVE EQUIPMENT S.A. Plan de inversión nueva línea de producción. Guadalajara. 952.743 €"));
-      expect((outputData.newsContent[15] as ParagraphStyledData).styledData[0],StyledString(text: "–ENVIROBAT ESPAÑA S.L. Adecuación, ampliación y mejora de la línea de reciclaje de pilas alcalinas. Azuqueca de Henares (Guadalajara). 310.362 €"));
+      expect((outputData.newsContent[11] as ParagraphStyledData).styledData[0],StyledString(text: "– GRAFICAS IMPAR S.L. Inversiones en bienes de equipo para la mejora y ampliación de líneas de producción. Toledo. 154.400 €"));
+      expect((outputData.newsContent[14] as ParagraphStyledData).styledData[0],StyledString(text: "– LCN AUTOMOTIVE EQUIPMENT S.A. Plan de inversión nueva línea de producción. Guadalajara. 952.743 €"));
+      expect((outputData.newsContent[15] as ParagraphStyledData).styledData[0],StyledString(text: "– ENVIROBAT ESPAÑA S.L. Adecuación, ampliación y mejora de la línea de reciclaje de pilas alcalinas. Azuqueca de Henares (Guadalajara). 310.362 €"));
       expect(outputData.newsContent.length, 21);
 
 //      expect((outputData.newsContent.length),4);
@@ -32,7 +32,7 @@ void main(){
     outputData = await SpiderSingleMethods.setUpScrapping(spiderSingleNews);
 
     expect((outputData.newsContent[0] as ParagraphStyledData).styledData[0],StyledString(text: "El Jefe de Digestivo del Complejo Hospitalario de Albacete, Ricardo Pérez Flores, ha fallecido en las últimas horas a consecuencia del coronavirus COVID-19 Este prestigioso doctor tenía 62 años de edad y padecía patologías previas, aunque en la última semana permanecía ingresado en el Hospital de Albacete. El Dr. Pérez Flores ejercía también la docencia en la Facultad de Medicina de la UCLM en Albacete."));
-    expect(outputData.newsContent.length, 21);
+    expect(outputData.newsContent.length, 8);
 
 //      expect((outputData.newsContent.length),4);
   });
