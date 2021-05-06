369152d56f2b909981a5a4722bd0dd0085a74cb6
Carlos Víllora Tercero <carlosvt@unicarlos.com>
2021-05-06 15:28:06 +0200

    Add jpeg image support

--- lib/tools/web_regex.dart
+++ lib/tools/web_regex.dart
@@ -3,7 +3,7 @@ class WebRegex{
   /// Example: background-image: url(https://www.eldigitaldealbacete.com/wp-content/uploads/2020/11/gente_calle_rosario_251020_archivo_albacete-30.jpg)
   static String getUrlFromStyleRegex(String style){
     return RegExp(
-          r"https://www\.eldigitaldealbacete\.com.*?\.(jpg|png|gif)")
+          r"https://www\.eldigitaldealbacete\.com.*?\.(jpg|jpeg|png|gif)")
           .firstMatch(style).group(0);
   }
 
